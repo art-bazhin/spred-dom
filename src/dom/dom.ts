d191ddb6164f89b54cb7d4d277020de4f80c2f07
art-bazhin <bazhin.artyom@yandex.ru>
2022-07-26 02:16:40 +0300

    refactor: remove deferred cleanup

--- src/dom/dom.ts
+++ src/dom/dom.ts
@@ -1,7 +1,5 @@
 const SUBS_KEY = '$subs';
 
-let cleanupQueue: (() => any)[][] = [];
-
 export function insertBefore(child: Node, mark: Node) {
   const parent = mark.parentNode;
 
@@ -18,13 +16,11 @@ export function removeNodes(start: Node, end: Node) {
   let next: Node | null = null;
 
   while (current && current !== end) {
-    addSubsToQueue(current);
+    cleanupSubs(current);
     next = current.nextSibling;
     parent.removeChild(current);
     current = next;
   }
-
-  cleanup();
 }
 
 export function isFragment(node: Node): node is DocumentFragment {
@@ -42,26 +38,18 @@ export function addSub(node: Node, sub: () => any) {
   subs.push(sub);
 }
 
-function addSubsToQueue(node: Node) {
+function cleanupSubs(node: Node) {
   const subs: (() => void)[] = (node as any)[SUBS_KEY];
+
+  if (subs) {
+    for (let unsub of subs) unsub();
+    delete (node as any)[SUBS_KEY];
+  }
 
   let child = node.firstChild;
 
   while (child) {
-    addSubsToQueue(child);
+    cleanupSubs(child);
     child = child.nextSibling;
   }
-
-  if (!subs) return;
-  cleanupQueue.push(subs);
-}
-
-function cleanup() {
-  setTimeout(() => {
-    for (let arr of cleanupQueue) {
-      for (let unsub of arr) unsub();
-    }
-
-    cleanupQueue = [];
-  });
 }
