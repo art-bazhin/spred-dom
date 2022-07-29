a8a5650d7f137702384bd66f0de206cee9194693
art-bazhin <bazhin.artyom@yandex.ru>
2022-07-29 20:52:08 +0300

    fix: remove bind function

--- src/bind/bind.ts
+++
@@ -1,9 +0,0 @@
-import { watch } from 'spred';
-import { createBinding } from '../create-binding/create-binding';
-import { addCleanup } from '../dom/dom';
-
-export function bind(fn: () => any) {
-  createBinding((mark) => {
-    addCleanup(mark, watch(fn));
-  });
-}
--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,6 @@ import { h } from './h/h';
 import { text } from './text/text';
 import { node } from './node/node';
 import { list } from './list/list';
-import { bind } from './bind/bind';
 import { cleanup } from './cleanup/cleanup';
 
-export {
-  h,
-  createComponent,
-  createComponentFn,
-  text,
-  node,
-  list,
-  bind,
-  cleanup,
-};
+export { h, createComponent, createComponentFn, text, node, list, cleanup };
