edd7611fa630c07ae0a28d14dc21a6414b4ec49e
art-bazhin <bazhin.artyom@yandex.ru>
2022-07-29 20:59:51 +0300

    feat: support multiple arguments in cleanup

--- src/cleanup/cleanup.ts
+++ src/cleanup/cleanup.ts
@@ -1,7 +1,14 @@
 import { createBinding } from '../create-binding/create-binding';
 import { addCleanup } from '../dom/dom';
 
-export function cleanup(fn: () => any) {
+export function cleanup(fn: () => any, ...callbacks: (() => any)[]) {
+  if (callbacks.length) {
+    callbacks.unshift(fn);
+    fn = () => {
+      for (let cb of callbacks) cb();
+    };
+  }
+
   createBinding((mark) => {
     addCleanup(mark, fn);
   });
